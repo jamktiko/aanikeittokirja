69ece54faa82870bbafc53a2f520f222bfb84a05
Ilmari Metsävainio <ilmevavi@gmail.com>
2022-09-27 12:39:06 +0300

    Lisäsin connection.js jolla yhdistetään RDS:n

--- express/app.js
+++ express/app.js
@@ -8,6 +8,11 @@ app.get('/', (req, res) => {
   res.send('Welcome to root URL of Server');
 });
 
+app.get('/test', (req, res) => {
+  res.status(200);
+  res.send('Test complete');
+});
+
 app.listen(PORT, (error) => {
   if (!error) console.log('Server is running on port ' + PORT);
   else console.log("Error occurred, server can't start", error);
