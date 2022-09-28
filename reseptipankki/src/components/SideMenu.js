e386d05acf7caea4c1300d76e64f6518aa4bbc38
Ville, Kuusela <AA2338@student.jamk.fi>
2022-09-28 11:04:09 +0300

    Sivuvalikon pyyhkäiseminen esiin

--- reseptipankki/src/components/SideMenu.js
+++ reseptipankki/src/components/SideMenu.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import { React, useState } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { motion } from 'framer-motion';
@@ -10,6 +10,28 @@ Tässä komponentissa on määritelty sivuvalikko, joka avataan NavBarissa oleva
 ikonista. Sivuvalikossa on sovelluksen navigaatio, sekä käyttäjätunnus.
 */
 const SideMenu = ({ toggleMenu }) => {
+  // Näillä lasketaan kosketuksia swaippausta varten.
+  const [touchStart, setTouchStart] = useState(null);
+  const [touchEnd, setTouchEnd] = useState(null);
+  const minSwipeDistance = 100;
+
+  // Nämä kolme funktiota hoitavat swaippausten kosketuksiin liityvät laskennat.
+  const onTouchStart = (e) => {
+    setTouchEnd(null);
+    setTouchStart(e.targetTouches[0].clientX);
+  };
+  const onTouchMove = (e) => {
+    setTouchEnd(e.targetTouches[0].clientX);
+  };
+  const onTouchEnd = () => {
+    if (!touchStart || !touchEnd) return;
+    const distance = touchStart - touchEnd;
+    const isLeftSwipe = distance < minSwipeDistance;
+    if (isLeftSwipe) {
+      toggleMenu();
+    }
+  };
+
   return (
     <div>
       <DarkBG toggle={toggleMenu} />
@@ -22,6 +44,14 @@ const SideMenu = ({ toggleMenu }) => {
         exit={{ x: 500 }}
         className="sideMenu"
       >
+        {/* Tämä div määrittää alueen jolla swaippaus toimii. */}
+        <div
+          className="swipeableZoneMenuOpen"
+          onTouchStart={onTouchStart}
+          onTouchMove={onTouchMove}
+          onTouchEnd={onTouchEnd}
+        />
+
         <Link onClick={() => toggleMenu()} to="/">
           Etusivu
         </Link>
