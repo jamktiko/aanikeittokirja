2c83a6a6df584e5ef0777145b691acc00618ba4a
Ville, Kuusela <AA2338@student.jamk.fi>
2022-09-29 12:44:26 +0300

    värejä ja button-komponentti

--- reseptipankki/src/components/FrontPage.js
+++ reseptipankki/src/components/FrontPage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Button from './Button';
 
 /*
 Etusivun komponentti. Sisältää tervehdyksen käyttäjälle,
@@ -8,7 +9,15 @@ suositeltujen reseptien listan.
 const FrontPage = () => {
   return (
     <div>
-      <p>Etusivu</p>
+      Etusivu
+      <div>
+        <h2>Napit</h2>
+        <Button color="primary" text="primary" />
+        <br />
+        <Button color="secondary" text="secondary" />
+        <br />
+        <Button color="warning" text="warning" />
+      </div>
     </div>
   );
 };
