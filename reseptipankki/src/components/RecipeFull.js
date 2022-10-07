5b593e452304f4c28982d83e460246ed4c2db2b0
Ville, Kuusela <AA2338@student.jamk.fi>
2022-10-07 13:38:35 +0300

    säätöä

--- reseptipankki/src/components/RecipeFull.js
+++ reseptipankki/src/components/RecipeFull.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Loading from './Loading';
 import LoadingError from './LoadingError';
+import '../styles/RecipeFull.css';
 
+import { BiDotsVerticalRounded } from 'react-icons/bi';
 import fetchRecipes from '../hooks/fetchRecipes';
 
-/*
-Reseptinäkymä, eli sivu jossa on yhden reseptin kaikki tiedot yms.
-*/
+// Reseptinäkymä, eli sivu jossa on yhden reseptin kaikki tiedot yms.
 const RecipeFull = () => {
   // Reseptin ID saadaan URL:n lopusta.
   const recipeId = window.location.href.substring(
@@ -22,10 +22,37 @@ const RecipeFull = () => {
   // Jos hook palauttaa virheen, näytetään LoadingError-komponentti.
   if (error) return <LoadingError />;
 
+  console.log(data);
+
   return (
     <div>
-      <h2>Reseptinäkymä</h2>
-      <p>Nimi: {data?.nimi}</p>
+      <div className="recipeContainer">
+        <div className="recipeImageContainer">
+          <img className="recipeImage" src={data?.kuva} />
+        </div>
+
+        <div className="recipeTitleContainer">
+          <h2>
+            {data?.nimi}{' '}
+            <span className="recipeTime">{`(${data?.valmistusaika})`}</span>
+          </h2>
+
+          <BiDotsVerticalRounded />
+        </div>
+
+        <div className="ingredientsContainer">
+          <h3>
+            Ainekset <span>{`(${data?.annosten_maara} annosta)`}</span>
+          </h3>
+          {/* Tähän ainekset kun ne saadaan backendistä */}
+          <p>...</p>
+        </div>
+
+        <div className="directionsContainer">
+          <h3>Ohjeet</h3>
+          <p>{data?.ohjeet}</p>
+        </div>
+      </div>
     </div>
   );
 };
--- reseptipankki/src/hooks/fetchRecipes.js
+++ reseptipankki/src/hooks/fetchRecipes.js
@@ -2,10 +2,12 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 /*
-Hook joka hakee reseptejä backendistä. recipeId on valinnainen
-parametri. Jos sen lisää, haetaan vain kyseisen ID:n mukainen resepti.
+Hook joka hakee kaikki reseptejä backendistä. Parametrillä
+voidaan tarkentaa hakua:
+- id: hook hakee vain kyseisen id:n reseptin.
+- public: hook hakee vain julkiset reseptit.
 */
-const fetchRecipes = (recipeId) => {
+const fetchRecipes = (param) => {
   const [data, setData] = useState(null); // Hausta palautuva data.
   const [loading, setLoading] = useState(false); // Tieto, onko haku käynnissä.
   const [error, setError] = useState(null); // Haun mahdollinen virheviesti.
@@ -13,14 +15,25 @@ const fetchRecipes = (recipeId) => {
   useEffect(() => {
     setLoading(true);
     axios
-      .get(`${process.env.REACT_APP_BACKEND_URL}/api/resepti/${recipeId}`)
+      .get(`${process.env.REACT_APP_BACKEND_URL}/api/resepti/${param}`, {
+        params: {
+          hakusana: 'kaali',
+          erikoisruokavaliot: {
+            kasvissyoja: 1,
+            maidoton: 1,
+          },
+        },
+      })
       .then((res) => {
+        console.log('found res.data: ', res.data);
         setData(res.data);
       })
       .catch((error) => {
+        console.log('FOUND ERROR: ', error);
         setError(error);
       })
       .finally(() => {
+        console.log('done');
         setLoading(false);
       });
   }, []);
