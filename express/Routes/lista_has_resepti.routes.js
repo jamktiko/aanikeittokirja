5ee7ea3c94fd2b3e98e50a4592fd8255685e6407
Toni Hiidenvuo <aa3689@student.jamk.fi>
2022-10-10 12:42:15 +0300

    reitit luotu

---
+++ express/Routes/lista_has_resepti.routes.js
@@ -0,0 +1,27 @@
+/*
+Route hallitsee sitä mitä metodeja käytetään controllereissa URL osoitteen perusteella.
+esimerkki URL GET http://localhost:3000/api/lista_has_resepti/ hakee kaikki listoihin liitetyt reseptit
+*/
+module.exports = (app) => {
+  const lista_has_resepti = require('../controllers/lista.controller.js');
+
+  const router = require('express').Router();
+
+  // Luo uusi lista
+  router.post('/', lista_has_resepti.create);
+
+  // Hae kaikki listat
+  router.get('/', lista_has_resepti.findAll);
+
+  // Hae lista id:n perusteella
+  router.get('/:id', lista_has_resepti.findOne);
+
+  // Päivitä lista id:n perusteella
+  router.put('/:id', lista_has_resepti.update);
+
+  // Poista lista id:n perusteella
+  router.delete('/:id', lista_has_resepti.delete);
+
+  // Tämä tulee kaikkien muiden muuttujien eteen
+  app.use('/api/lista_has_resepti', router);
+};
