cb383cb505c0db330d48a4f84a4c81084b86528c
Toni Hiidenvuo <aa3689@student.jamk.fi>
2022-10-10 12:39:54 +0300

    lisäys-metodi luotu

---
+++ express/Controllers/lista_has_resepti.controller.js
@@ -0,0 +1,29 @@
+/* 
+Contoller käyttää modelin metodeja ja käsittelee niiden palauttamia arvoja.
+*/
+
+const Lista_has_resepti = require('../models/lista_has_resepti.model.js');
+
+// Luo uusi lista_has_resepti
+exports.create = (req, res) => {
+  if (!req.body) {
+    res.status(400).send({
+      message: 'Body cannot be empty!',
+    });
+  }
+
+  // Lista_l_id on listan id, johon resepti liitetään,
+  // Resepti_r_id on reseptin id, joka liitetään listaan
+  const lista_has_resepti = new Lista_has_resepti({
+    Lista_l_id: req.body.Lista_l_id,
+    Resepti_r_id: req.body.Resepti_r_id,
+  });
+
+  Lista_has_resepti.create(lista_has_resepti, (err, data) => {
+    if (err)
+      res.status(500).send({
+        message: err.message || 'An error while creating lista_has_resepti',
+      });
+    else res.send(data);
+  });
+};
