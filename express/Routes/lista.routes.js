dfe05357fe97d5ee5988aac23e65d9c33c7f4ae8
Toni Hiidenvuo <aa3689@student.jamk.fi>
2022-10-10 11:27:08 +0300

    korjattu kommentteja

--- express/Routes/lista.routes.js
+++ express/Routes/lista.routes.js
@@ -1,6 +1,6 @@
 /*
 Route hallitsee sitä mitä metodeja käytetään controllereissa URL osoitteen perusteella.
-esimerkki URL GET http://localhost:3000/api/lista/ hakee kaikki käyttäjät
+esimerkki URL GET http://localhost:3000/api/lista/ hakee kaikki listat
 */
 module.exports = (app) => {
   const lista = require('../controllers/lista.controller.js');
@@ -10,18 +10,18 @@ module.exports = (app) => {
   // Luo uusi lista
   router.post('/', lista.create);
 
-  // hae kaikki listat
+  // Hae kaikki listat
   router.get('/', lista.findAll);
 
   // Hae lista id:n perusteella
   router.get('/:id', lista.findOne);
 
-  // Päivitä lista ID:n perusteella
+  // Päivitä lista id:n perusteella
   router.put('/:id', lista.update);
 
-  // Poista lista ID:n perusteella
+  // Poista lista id:n perusteella
   router.delete('/:id', lista.delete);
 
-  //Tämä tulee kaikkien muiden muuttujien eteen
+  // Tämä tulee kaikkien muiden muuttujien eteen
   app.use('/api/lista', router);
 };
