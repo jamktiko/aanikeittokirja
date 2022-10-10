92ac711a3340a57a048c4e224dc62e80f7d3526e
Toni Hiidenvuo <aa3689@student.jamk.fi>
2022-10-10 13:39:40 +0300

    typo

--- express/Routes/lista_has_resepti.routes.js
+++ express/Routes/lista_has_resepti.routes.js
@@ -7,19 +7,19 @@ module.exports = (app) => {
 
   const router = require('express').Router();
 
-  // Luo uusi lista
+  // Luo uusi lista_has_resepti
   router.post('/', lista_has_resepti.create);
 
-  // Hae kaikki listat
+  // Hae kaikki lista_has_reseptit
   router.get('/', lista_has_resepti.findAll);
 
-  // Hae lista id:n perusteella
+  // Hae lista_has_resepti id:n perusteella
   router.get('/:id', lista_has_resepti.findOne);
 
-  // Päivitä lista id:n perusteella
+  // Päivitä lista_has_resepti id:n perusteella
   router.put('/:id', lista_has_resepti.update);
 
-  // Poista lista id:n perusteella
+  // Poista lista_has_resepti id:n perusteella
   router.delete('/:id', lista_has_resepti.delete);
 
   // Tämä tulee kaikkien muiden muuttujien eteen
