f55a59eb2efa5713c890a12c989ccbf03f0f8f6d
Toni Hiidenvuo <aa3689@student.jamk.fi>
2022-10-10 16:22:55 +0300

    reitit luotu

---
+++ express/Routes/kalenteri_item.routes.js
@@ -0,0 +1,27 @@
+/*
+Route hallitsee sitä mitä metodeja käytetään controllereissa URL osoitteen perusteella.
+esimerkki URL GET http://localhost:3000/api/kalenteri_item/ hakee kaikki kalentereihin liitetyt reseptit
+*/
+module.exports = (app) => {
+  const kalenteri_item = require('../controllers/kalenteri_item.controller.js');
+
+  const router = require('express').Router();
+
+  // Luo uusi kalenteri_item
+  router.post('/', kalenteri_item.create);
+
+  // Hae kaikki kalenteri_itemit
+  router.get('/', kalenteri_item.findAll);
+
+  // Hae kalenteri_item id:n perusteella
+  router.get('/:id', kalenteri_item.findOne);
+
+  // Päivitä kalenteri_item id:n perusteella
+  router.put('/:id', kalenteri_item.update);
+
+  // Poista kalenteri_item id:n perusteella
+  router.delete('/:id', kalenteri_item.delete);
+
+  // Tämä tulee kaikkien muiden muuttujien eteen
+  app.use('/api/kalenteri_item', router);
+};
