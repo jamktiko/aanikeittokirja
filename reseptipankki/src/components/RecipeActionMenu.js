a3f5f293861d81855def54b4fc5136d85fb270d2
Ville, Kuusela <AA2338@student.jamk.fi>
2022-10-11 12:22:29 +0300

    res.toim.valikon korjauksia

--- reseptipankki/src/components/RecipeActionMenu.js
+++ reseptipankki/src/components/RecipeActionMenu.js
@@ -6,16 +6,18 @@ import RecipeActionMenuContent from './RecipeActionMenuContent';
 
 const RecipeActionMenu = () => {
   return (
-    <motion.div
-      key="recipeActionMenu"
-      initial={{ y: 500 }} // Näkymän sijainti ennen animaatiota
-      animate={{ y: 0 }} // Näkymän sijainti animaation jälkeen
-      transition={{ duration: 0.3, ease: 'easeOut' }} // Kesto ja pehmennys
-      exit={{ y: 500 }} // Sijainti johon näkymää menee kadotessaan.
-      className="recipeActionMenu"
-    >
-      <RecipeActionMenuContent />
-    </motion.div>
+    <div className="revipeActionMenuContainer">
+      <motion.div
+        key="recipeActionMenu"
+        initial={{ y: 500 }} // Näkymän sijainti ennen animaatiota
+        animate={{ y: 0 }} // Näkymän sijainti animaation jälkeen
+        transition={{ duration: 0.3, ease: 'easeOut' }} // Kesto ja pehmennys
+        exit={{ y: 500 }} // Sijainti johon näkymää menee kadotessaan.
+        className="recipeActionMenu"
+      >
+        <RecipeActionMenuContent />
+      </motion.div>
+    </div>
   );
 };
 
