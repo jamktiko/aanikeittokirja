e4a74423a016943f78c13e1eff61bb3e0dbefdf0
Ville, Kuusela <AA2338@student.jamk.fi>
2022-10-31 14:53:25 +0200

    Käyttäjä voi poistaa vain omia reseptejään

--- reseptipankki/src/components/RecipeActionMenuContent.js
+++ reseptipankki/src/components/RecipeActionMenuContent.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-import { React, useState } from 'react';
+import { React, useState, useEffect } from 'react';
 import Button from './Button';
 import PropTypes from 'prop-types';
 import { useNavigate, Link } from 'react-router-dom';
@@ -16,6 +16,9 @@ const RecipeActionMenuContent = ({ recipeData, ingredientsData }) => {
   // Tila siitä onko reseptin poistamisvalikko auki.
   const [deleteOptionOpen, toggleOpen] = useState(false);
 
+  // Käyttäjän RDS-tietokannasta saatavat tiedot laitetaan tähän tilaan:
+  const [rdsAccount, setRdsAccount] = useState();
+
   // Funktio joka poistaa reseptin.
   const deleteRecipe = () => {
     axios
@@ -30,6 +33,28 @@ const RecipeActionMenuContent = ({ recipeData, ingredientsData }) => {
         console.error(error);
       });
   };
+
+  // UseEffectissä ladataan käyttäjän k_id, jotta voidaan
+  // tarkistaa onko resepti käyttäjän oma vai jonkun muun.
+  useEffect(() => {
+    // Ladataan käyttäjätiedot localStoragesta...
+    const userData = localStorage.getItem('user');
+    // ...ja muunnetaan ne takaisin objektiksi...
+    const parsedData = JSON.parse(userData);
+    // ...josta saadaan cognito_id, millä voidaan hakea
+    // käyttäjän ID rds-tietokannassa.
+    axios
+      .get(
+        // eslint-disable-next-line max-len
+        `${process.env.REACT_APP_BACKEND_URL}/api/kayttaja/cid/"${parsedData?.idToken.payload['cognito:username']}"`
+      )
+      .then((res) => {
+        setRdsAccount(res.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
 
   return (
     <div className="recipeActionMenuContent">
@@ -61,35 +86,49 @@ const RecipeActionMenuContent = ({ recipeData, ingredientsData }) => {
 
       <div className="divider" />
 
-      {deleteOptionOpen ? (
+      {/*
+      - Jos reseptin lisännyt käyttäjä on sama kuin joka on kirjautunut
+      sovellukseen, näytetään reseptin poistamisnappi.
+      - Jos resepti ei ole kirjautuneen käyttäjän lisäämä, näytetään sen
+      paikalla "Lisää omiin resepteihin" -nappi. Sitä ei tarvitse näyttää
+      käyttäjän omissa resepteissä, sillä käyttäjän itse lisäämät reseptit
+      löytyvät aina hänen Omista Resepteistään.
+      */}
+      {rdsAccount && rdsAccount[0].k_id === recipeData?.Kayttaja_k_id ? (
         <div>
-          <p>Haluatko varmasti poistaa reseptin?</p>
-          <div className="twoButtonsDiv">
-            <div onClick={() => toggleOpen(!deleteOptionOpen)}>
-              <Button color={'secondary'} text={'Peruuta'} />
-            </div>
+          {deleteOptionOpen ? (
+            <div>
+              <p>Haluatko varmasti poistaa reseptin?</p>
+              <div className="twoButtonsDiv">
+                <div onClick={() => toggleOpen(!deleteOptionOpen)}>
+                  <Button color={'secondary'} text={'Peruuta'} />
+                </div>
 
-            <div onClick={() => deleteRecipe()}>
-              <Button color={'warning'} text={'Poista'} />
+                <div onClick={() => deleteRecipe()}>
+                  <Button color={'warning'} text={'Poista'} />
+                </div>
+              </div>
             </div>
-          </div>
+          ) : (
+            <button
+              className="buttonInvisible width100"
+              onClick={() => toggleOpen(!deleteOptionOpen)}
+            >
+              <p>Poista</p>
+            </button>
+          )}
+
+          <div className="divider" />
         </div>
       ) : (
-        <button
-          className="buttonInvisible width100"
-          onClick={() => toggleOpen(!deleteOptionOpen)}
-        >
-          <p>Poista</p>
-        </button>
+        <div>
+          <button className="buttonInvisible width100">
+            <p>Lisää omiin resepteihin</p>
+          </button>
+
+          <div className="divider" />
+        </div>
       )}
-
-      <div className="divider" />
-
-      <button className="buttonInvisible width100">
-        <p>Lisää omiin resepteihin</p>
-      </button>
-
-      <div className="divider" />
 
       <button className="buttonInvisible width100">
         <p>Lisää listalle</p>
