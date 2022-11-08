a56a8288a7139f26af0f497a85307768ae35967d
Toni Luomala <aa4498@student.jamk.fi>
2022-11-08 15:24:08 +0200

    Pieniä lisäyksiä lista komponenttiin ja lisätty Modal komponentti

---
+++ reseptipankki/src/components/Modal.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Modal() {
+  return (
+    <div>
+      <div className="modalBackground"></div>
+      <div className="modalContainer">
+        <button>Sulje</button>
+      </div>
+    </div>
+  );
+}
+
+export default Modal;
