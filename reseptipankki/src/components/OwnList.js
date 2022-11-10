d15a634a1635e4b7524fd82d0c12de543de1430a
Toni Luomala <aa4498@student.jamk.fi>
2022-11-10 14:06:13 +0200

    listan reseptien haku

--- reseptipankki/src/components/OwnList.js
+++ reseptipankki/src/components/OwnList.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import fetchRecipesinList from '../hooks/fetchRecipesinList';
+import Loading from './Loading';
+import LoadingError from './LoadingError';
 
 const OwnList = () => {
+  // Reseptin ID saadaan URL:n lopusta.
+  const listId = window.location.href.substring(
+    window.location.href.lastIndexOf('/') + 1
+  );
+  // Reseptien hakeminen hookilla. Vain ID:n mukainen resepti haetaan.
+  const { data, loading, error } = fetchRecipesinList(listId);
+
+  console.log(data);
+
+  // Kun hookin lataus on kesken, näytetään Loading-komponentti.
+  if (loading) return <Loading />;
+
+  // Jos hook palauttaa virheen, näytetään LoadingError-komponentti.
+  if (error) {
+    return <LoadingError subtext="Listan reseptin hakeminen epäonnistui" />;
+  }
+
+  console.log(data);
+  console.log(listId);
+
   return (
     <div>
-      <p>Lista</p>
+      <div>
+        <p>Lista</p>
+      </div>
+      <div>{(data, loading, error)}</div>
     </div>
   );
 };
