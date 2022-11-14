f2d6b4cc8387ca82f3dae19dc4d43c6ea4b52153
Ilmari Metsävainio <ilmevavi@gmail.com>
2022-11-14 14:27:22 +0200

    Turhia osia poistettu sekä käyytäjän tarkistus lisätty.

--- express/Controllers/ostoslista_aines.controller.js
+++ express/Controllers/ostoslista_aines.controller.js
@@ -4,7 +4,8 @@ Contoller käyttää modelin metodeja ja käsittelee niiden palauttamia arvoja.
 
 const conn = require('../connection.js');
 const Ostoslista_aines = require('../models/ostoslista_aines.model.js');
-
+const Ostoslista = require('../models/ostoslista.model');
+const Kayttaja = require('../models/kayttaja.model');
 // Luo uusi aines
 /* tieto tänne muodossa 
 {
@@ -18,37 +19,71 @@ exports.create = (req, res) => {
       message: 'Body cannot be empty!',
     });
   }
-  const ainekset = req.body.ainekset;
-  //Tässä käytetään transaktiota varmuuden vuoksi
-  conn.beginTransaction(function (err) {
-    let final;
-    ainekset.forEach((aines) => {
-      const AinesData = new Ostoslista_aines({
-        aines: aines.aines,
-        maara: aines.maara,
-        yksikko: aines.yksikko,
-        Ostoslista_o_id: req.body.Ostoslista_o_id,
-      });
-      Ostoslista_aines.create(AinesData, (err, data) => {
-        if (err) {
-          conn.rollback(function () {
-            throw err;
-          });
-        }
-        final += data;
-      });
-    });
-    conn.commit(function (err) {
-      console.log('commit');
-      if (err) {
-        conn.rollback(function () {
-          throw err;
+  // Haetaan lista johon resepti lisätään että saadaan sen omistajan id
+  Ostoslista.findById(req.body.Ostoslista_o_id, (err, data) => {
+    if (err) {
+      if (err.kind === 'not_found') {
+        res.status(404).send({
+          message: 'Lista not found',
         });
       } else {
-        console.log('Successfully added ingredients to shopping bag');
-        res.send(final);
+        res.status(500).send({
+          message: 'Search error.',
+        });
       }
-    });
+    } else {
+      // omistajan id:n perusteella haetaan omistaja että saadaan hänen cognito_id
+      Kayttaja.findById(data.Kayttaja_k_id, (err, data) => {
+        if (err)
+          res
+            .status(500)
+            .send({ message: err.message || 'Error getting user' });
+        else {
+          user = data.cognito_id;
+          //Testataan että lisääjä on sama kuin listan omistaja että joku muu ei voi editoida toisten listoja.
+          if (user !== req.headers.cognitoid) {
+            res
+              .status(401)
+              .send({ message: 'You can not create things for other users' });
+            return;
+          }
+          const ainekset = req.body.ainekset;
+          //Tässä käytetään transaktiota varmuuden vuoksi
+          conn.beginTransaction(function (err) {
+            let final;
+            ainekset.forEach((aines) => {
+              const AinesData = new Ostoslista_aines({
+                aines: aines.aines,
+                maara: aines.maara,
+                yksikko: aines.yksikko,
+                Ostoslista_o_id: req.body.Ostoslista_o_id,
+              });
+              Ostoslista_aines.create(AinesData, (err, data) => {
+                if (err) {
+                  conn.rollback(function () {
+                    throw err;
+                  });
+                }
+                final += data;
+              });
+            });
+            conn.commit(function (err) {
+              console.log('commit');
+              if (err) {
+                conn.rollback(function () {
+                  throw err;
+                });
+              } else {
+                console.log('Successfully added ingredients to shopping list');
+                res.send({
+                  message: 'Successfully added ingredients to shopping list',
+                });
+              }
+            });
+          });
+        }
+      });
+    }
   });
 };
 
@@ -111,48 +146,78 @@ exports.update = (req, res) => {
       message: 'Body cannot be empty!',
     });
   }
-
-  console.log(req.body);
-
-  Ostoslista_aines.updateById(
-    req.params.id,
-    new Ostoslista_aines(req.body),
-    (err, data) => {
-      if (err) {
-        if (err.kind === 'not_found') {
-          res.status(404).send({
-            message: `Not found ingredient with id ${req.params.id}.`,
-          });
-        } else {
-          res.status(500).send({
-            message: 'Error updating ingredient with id ' + req.params.id,
-          });
+  Ostoslista.findById(req.body.Ostoslista_o_id, (err, data) => {
+    if (err) {
+      if (err.kind === 'not_found') {
+        res.status(404).send({
+          message: 'Lista not found',
+        });
+      } else {
+        res.status(500).send({
+          message: 'Search error.',
+        });
+      }
+    } else {
+      // omistajan id:n perusteella haetaan omistaja että saadaan hänen cognito_id
+      Kayttaja.findById(data.Kayttaja_k_id, (err, data) => {
+        if (err)
+          res
+            .status(500)
+            .send({ message: err.message || 'Error getting user' });
+        else {
+          user = data.cognito_id;
+          //Testataan että lisääjä on sama kuin listan omistaja että joku muu ei voi editoida toisten listoja.
+          if (user !== req.headers.cognitoid) {
+            res
+              .status(401)
+              .send({ message: 'You can not create things for other users' });
+            return;
+          }
+          Ostoslista_aines.updateById(
+            req.params.id,
+            new Ostoslista_aines(req.body),
+            (err, data) => {
+              if (err) {
+                if (err.kind === 'not_found') {
+                  res.status(404).send({
+                    message: `Not found ingredient with id ${req.params.id}.`,
+                  });
+                } else {
+                  res.status(500).send({
+                    message:
+                      'Error updating ingredient with id ' + req.params.id,
+                  });
+                }
+              } else res.send(data);
+            }
+          );
         }
-      } else res.send(data);
+      });
     }
-  );
+  });
 };
 
 // Poista aines aineksen id:n perusteella
 exports.delete = (req, res) => {
-  Ostoslista_aines.remove(req.params.id, (err, data) => {
-    if (err) {
-      res.status(500).send({
-        message: 'Error deleting ingredient with id ' + req.params.id,
+  Kayttaja.findById(req.params.Kayttaja_k_id, (err, data) => {
+    if (err)
+      res.status(500).send({ message: err.message || 'Error getting user' });
+    else {
+      user = data.cognito_id;
+      //Testataan että lisääjä on sama kuin listan omistaja että joku muu ei voi editoida toisten listoja.
+      if (user !== req.headers.cognitoid) {
+        res
+          .status(401)
+          .send({ message: 'You can not create things for other users' });
+        return;
+      }
+      Ostoslista_aines.remove(req.params.id, (err, data) => {
+        if (err) {
+          res.status(500).send({
+            message: 'Error deleting ingredient with id ' + req.params.id,
+          });
+        } else res.send(data);
       });
-    } else res.send(data);
-  });
-};
-
-// Poista aines kun sille kuuluva resepti poistetaan
-exports.deleteByList = (req, res) => {
-  Ostoslista_aines.removeByList(req.params.id, (err, data) => {
-    if (err) {
-      res.status(500).send({
-        message:
-          'Error deleting ingredient with recipe id (r_id) ' +
-          req.params.Resepti_r_id,
-      });
-    } else res.send(data);
+    }
   });
 };
--- express/Routes/aines.routes.js
+++ express/Routes/aines.routes.js
@@ -8,26 +8,8 @@ module.exports = (app) => {
 
   const router = require('express').Router();
 
-  // Luo uusi Aines
-  router.post('/', validateAuth, Aines.create);
-
-  // hae kaikki Ainekset
-  router.get('/', Aines.findAll);
-
-  // Hae Aines id:n perusteella
-  router.get('/:id', Aines.findOne);
-
   // Hae ainekset reseptin ID:n perustella
   router.get('/resepti/:id', Aines.findByRecipe);
-
-  // Päivitä Aines ID:n perusteella
-  router.put('/:id', validateAuth, Aines.update);
-
-  // Poista Aines ID:n perusteella
-  router.delete('/:id', validateAuth, Aines.delete);
-
-  // Poista Aines reseptin ID:n perusteella
-  router.delete('/resepti/:id', validateAuth, Aines.delete);
 
   //Tämä tulee kaikkien muiden muuttujien eteen
   app.use('/api/aines', router);
