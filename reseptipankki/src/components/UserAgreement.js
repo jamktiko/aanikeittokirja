0641a3105de4d620a328a1062d3b1cf3c8a33aee
Ville, Kuusela <AA2338@student.jamk.fi>
2022-11-21 12:21:37 +0200

    käyttöehtojen sivu

---
+++ reseptipankki/src/components/UserAgreement.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Button from './Button';
+import PropTypes from 'prop-types';
+import '../styles/UserRegisterLoginPage.css';
+
+const UserAgreement = ({ togglePage }) => {
+  return (
+    <div className="userAgreementPageModal">
+      <div className="userAgreementPageContainer">
+        <h2>Britan käyttöehdot</h2>
+
+        <p>1. Lorem Ipsum</p>
+        <p>2. Lorem Ipsum</p>
+        <p>3. Lorem Ipsum</p>
+
+        <div onClick={() => togglePage(false)}>
+          <Button color="secondary" text="sulje" type="button" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+// Parametrien tyypitykset.
+UserAgreement.propTypes = {
+  togglePage: PropTypes.func,
+};
+
+export default UserAgreement;
