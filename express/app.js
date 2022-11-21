51540f48b74ef73ef3a29c4604706bf10087fca9
Toni Hiidenvuo <aa3689@student.jamk.fi>
2022-11-21 12:54:32 +0200

    process.env.port lisätty (beanstalk käyttää sitä)

--- express/app.js
+++ express/app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 require('./connection');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
