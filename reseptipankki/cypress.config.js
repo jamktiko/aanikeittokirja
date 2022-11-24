6a21050ef9c46ff48f8714f8ba426740f30bc2a7
Toni Luomala <aa4498@student.jamk.fi>
2022-11-24 12:59:31 +0200

    Uusi testi ja local storageen tallennuksen mahdollistavia muutoksia

--- reseptipankki/cypress.config.js
+++ reseptipankki/cypress.config.js
@@ -1,16 +1,18 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   component: {
     devServer: {
-      framework: "create-react-app",
-      bundler: "webpack",
-    },
+      framework: 'create-react-app',
+      bundler: 'webpack'
+    }
   },
 
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-    },
-  },
+      require('cypress-localstorage-commands/plugin')(on, config);
+      return config;
+    }
+  }
 });
