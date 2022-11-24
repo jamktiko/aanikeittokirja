752c4cffcd8d75c17f4d15f49152c8d0136f81e1
Ville, Kuusela <AA2338@student.jamk.fi>
2022-11-24 09:36:05 +0200

    reseptien lataamisen sivun pohja

--- reseptipankki/src/components/DownloadRecipe.js
+++ reseptipankki/src/components/DownloadRecipe.js
@@ -1,4 +1,7 @@
-import React from 'react';
+import { React, useState } from 'react';
+import '../styles/DownloadRecipe.css';
+import Button from './Button';
+import { BiPaste } from 'react-icons/bi';
 
 /*
 Komponentti näkymälle, jossa käyttäjä voi liittää kopioimansa linkin
@@ -6,7 +9,36 @@ tekstikenttään, sekä napin jonka painamisen jälkeen linkin takana
 oleva resepti latautuu.
 */
 const RecipeDownload = () => {
-  return <div>Lataa resepti</div>;
+  const [url, setUrl] = useState('');
+
+  const pasteClipboard = async () => {
+    const text = await navigator.clipboard.readText();
+    setUrl(text);
+  };
+
+  return (
+    <div className="downloadRecipeContainer">
+      <h2>Kopioi resepti netistä</h2>
+      <p>Liitä reseptin URL-osoite:</p>
+
+      <div className="searchBarContainer">
+        <input
+          value={url}
+          onChange={({ target }) => setUrl(target.value)}
+          className="searchBar"
+          type="text"
+        />
+
+        <div onClick={pasteClipboard} className="filterButton">
+          <BiPaste className="filterIcon" />
+        </div>
+      </div>
+
+      <div>
+        <Button color="primary" text="Kopioi" type="button" />
+      </div>
+    </div>
+  );
 };
 
 export default RecipeDownload;
