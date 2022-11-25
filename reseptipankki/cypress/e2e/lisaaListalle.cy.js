cb98cfb58e62001fd302b6173c94af1af3ccec58
Toni Luomala <aa4498@student.jamk.fi>
2022-11-25 15:25:00 +0200

    Lisaalistalle testin korjaus, kaikki vaiheet eri testin sisällä

--- reseptipankki/cypress/e2e/lisaaListalle.cy.js
+++ reseptipankki/cypress/e2e/lisaaListalle.cy.js
@@ -1,68 +1,93 @@
-describe('Reseptinhaku', () => {
-  it('Siirrytään sovellukseen url -osoitteessa 3001', () => {
-    cy.visit('localhost:3001');
+describe('Kirjautuminen, lisää listalle ja poista listalta', () => {
+  // Palauttaa local storagen käyttäjän kirjautumistiedot ennen jokaisen testin
+  // alkua.
+  beforeEach(() => {
+    cy.restoreLocalStorage();
   });
 
-  it('Avataan sivuvalikko klikkaamalla hampurilaisvalikon painiketta', () => {
+  // Tallentaa local storagen käyttäjän kirjautumistiedot jokaisen testin
+  // jälkeen.
+  afterEach(() => {
+    cy.saveLocalStorage();
+  });
+
+  it('Siirrytään sovellukseen url -osoitteessa 3001', () => {
+    cy.visit('localhost:3001');
+    // Tyhjentää local storagen, jotta kirjautumistiedot eivät säily muistissa
+    // edellisistä testeistä.
+    cy.clearLocalStorage();
+  });
+
+  it('Avaa sivuvalikko ja klikkaa hampurilaisvalikon painiketta', () => {
     cy.get('.buttonInvisible').click();
   });
 
-  it('Etsitään sivuvalikosta kirjautuminen ja klikataan sitä', () => {
+  it('Etsi sivuvalikosta kirjautuminen ja klikkaa sitä', () => {
     cy.contains('Kirjaudu').click();
   });
 
-  it('Siirrytään Sähköpostin input kenttään ja kirjoitetaan sähköposti', () => {
+  it('Siirry Sähköpostin input kenttään ja kirjoita sähköposti osoite', () => {
     cy.get('.accountFormRow').first().type('aa4498@student.jamk.fi');
   });
 
-  it('Siirrytään salasanan input kenttään ja kirjoitetaan salasana', () => {
+  it('Siirry salasanan input kenttään ja kirjoita salasana', () => {
     cy.get('.accountFormRow').last().type('salasana');
   });
 
-  it('Painetaan "kirjaudu sisään" -painiketta ', () => {
+  it('Paina "kirjaudu sisään" -painiketta ', () => {
     cy.get('.accountFormSubmitButton').click();
   });
 
-  it('Lisätään resepti listalle ja poistetaan se', () => {
-    // Haetaan sivuvalikon painike ja klikataan sitä
+  it('Haetaan sivuvalikon painike ja klikataan sitä', () => {
     cy.get('.buttonInvisible').click();
-
     cy.wait(2000);
+  });
 
-    // Etsi valikosta kirjoita resepti ja paina sitä
+  it('Etsi valikosta kirjoita resepti ja paina sitä', () => {
     cy.contains('Hae reseptejä').click();
+  });
 
-    // Etsitään resepteistä suklaakeksit ja klikataan sitä
+  it('Etsitään resepteistä suklaakeksit ja klikataan sitä', () => {
     cy.contains('Suklaakeksit').click();
+  });
 
-    // Haetaan valikkopainike ja klikataan sitä
+  it('Haetaan valikkopainike ja klikataan sitä', () => {
     cy.get('button[class="recipeActionMenuIcon buttonInvisible"]').click();
+  });
 
-    // Etsitään valikosta "Lisää listalle" ja klikataan
+  it('Etsitään valikosta "Lisää listalle" ja klikataan', () => {
     cy.contains('Lisää listalle').click();
+  });
 
-    // Etsitään "Tonin lista" ja klikataan
+  it('Etsitään "Tonin lista" ja klikataan', () => {
     cy.contains('Tonin lista').click();
+  });
 
-    // Haetaan valikkopainike ja klikataan
+  it('Haetaan valikkopainike ja klikataan', () => {
     cy.get('.menuIcon').click();
+  });
 
-    // Etsitään "Omat listat" valikosta ja klikataan
+  it('Etsitään "Omat listat" valikosta ja klikataan', () => {
     cy.contains('Omat listat').click();
+  });
 
-    // Haetaan valikon elementti ja klikataan
+  it('Haetaan valikon elementti ja klikataan', () => {
     cy.get('.listCardIcon').click();
+  });
 
-    // Haetaan painike ja klikataan
+  it('Haetaan painike ja klikataan', () => {
     cy.get('path').click();
+  });
 
-    // Etsitään "Poista reseptejä" painike ja klikataan
+  it('Etsitään "Poista reseptejä" painike ja klikataan', () => {
     cy.contains('Poista reseptejä').click();
+  });
 
-    // Etsitään "Suklaakeksit" ja klikataan
+  it('Etsitään "Suklaakeksit" ja klikataan', () => {
     cy.contains('Suklaakeksit').click();
+  });
 
-    // Etsitään "Poista listalta" ja klikataan
+  it('Etsitään "Poista listalta" ja klikataan', () => {
     cy.contains('Poista listalta').click();
   });
 });
