6f591ab488c17d3c1282add2e2ab7f95b0091b5f
Toni Luomala <aa4498@student.jamk.fi>
2022-12-01 09:17:59 +0200

    Pohjaa ostoslistalle

--- reseptipankki/src/components/ShoppingLists.js
+++ reseptipankki/src/components/ShoppingLists.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import '../styles/ShoppingLists.css';
 
 /*
 Näkymä käyttäjän ostoslistoille. Sisältää painikkee, josta uusi
 ostoslista voidaan luoda, sekä kaikki käyttäjän ostoslistat.
 */
 const ShoppingLists = () => {
-  return <div>Ostoslistat</div>;
+  // Tieto, onko lisäysikkuna näkyvissä
+
+  const addNew = () => {
+    setOpenModal(true);
+  };
+
+  return (
+    <div className="shoppingContainer">
+      <div className="headLine">
+        <div className="header">Ostoslistat</div>
+        <div>
+          <button onClick={addNew} className="buttonInvisible">
+            + UUSI LISTA
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ShoppingLists;
